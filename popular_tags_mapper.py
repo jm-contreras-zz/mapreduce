d7e5d955861faac067095215b6fab09f39a648ea
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-03-26 16:36:55 -0400

    Create popular_tags_mapper.py

---
+++ popular_tags_mapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Import relevant modules
+import sys
+import csv
+
+# Load the data
+reader = csv.reader(sys.stdin, delimiter='\t')
+
+# Skip the header
+reader.next()
+
+# For every line in the data...
+for line in reader:
+
+    # ...that has 19 fields...
+    if len(line) == 19:
+
+  	    # ...split the tag names by whitespace into a list
+        tagnames = line[2].split(' ')
+
+    	# ...and print each one as a key
+    	for key in tagnames:
+    	    print key
