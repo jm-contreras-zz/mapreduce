332a4cfba71ff33a0d9e370922901c7561f735bc
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-03-26 21:33:35 -0400

    Update combine_mapper.py

--- combine_mapper.py
+++ combine_mapper.py
@@ -16,11 +16,11 @@ for line in csv.reader(sys.stdin, delimiter='\t'):
     # ...if it is part of forum_users, which has 5 lines
     if len(line) == 5:
 
-    # ...write the relevant lines to the standard output
+    	# ...write the relevant lines to the standard output
 	writer.writerow([line[0]] + ['A'] + line[1:])
 
-    # ...if it is part of forum_node, which has 19 lines
-    elif len(line) == 19:
+	# ...if it is part of forum_node, which has 19 lines
+    	elif len(line) == 19:
 
-    # ...write the relevant lines to the standard output
+    	# ...write the relevant lines to the standard output
 	writer.writerow([line[3]] + ['B'] + line[:3] + line[5:10])
