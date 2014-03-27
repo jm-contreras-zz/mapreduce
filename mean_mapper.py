a04fd4e26b25b872c796fdbe04ef1f69c2635fce
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-03-26 20:50:14 -0400

    Create mean_mapper.py

---
+++ mean_mapper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# Import relevant modules
+import sys
+from datetime import datetime
+
+# For every line in the data...
+for line in sys.stdin:
+
+    # ...strip and split it
+    data = line.strip().split('\t')
+    
+    # ...if it has six values
+    if len(data) == 6:
+        
+        # ...unpack them
+        date, time, store, item, cost, payment = data
+        
+        # ...declare the day of the week
+	    day = datetime.strptime(date, '%Y-%m-%d').weekday()
+	    
+        print '{0}\t{1}'.format(day, cost)
