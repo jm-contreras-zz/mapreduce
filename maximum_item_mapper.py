88b7686150ac2328622b1291c83ae6d2553656c6
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-03-26 21:06:09 -0400

    Create maximum_item_mapper.py

---
+++ maximum_item_mapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Import relevant module
+import sys
+
+# For every line in the data...
+for line in sys.stdin:
+
+    # ...strip and split it
+    data = line.strip().split('\t')
+
+    # ...if it has six values
+    if len(data) == 6:
+
+        # ...unpack them
+        date, time, store, item, cost, payment = data
+
+        # ...print the key-value pair
+        print '{0}\t{1}'.format(item, cost)
